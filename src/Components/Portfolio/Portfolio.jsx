1f567c96408453282a582191f50fc93b729ae0e3
Anil <ak26122k@gmail.com>
2023-08-16 00:05:25 +0530

    project link add

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -13,22 +13,31 @@ const Portfolio = () => {
       </div>
       <div className="project">
         <div>
-          <a href="">
-            <img src={Socialmedia} alt="" />
-          </a>
-          <div>Socila Media Website With MERN Stack</div>
-        </div>
-        <div>
-          <a href="">
+          <a
+            href="https://creative-praline-c065c8.netlify.app/"
+            target="_blank"
+          >
             <img src={Ecommerce} alt="" />
           </a>
           <div>Ecommerce Website With MERN Stack</div>
         </div>
         <div>
-          <a href="">
+          <a
+            href="https://tranquil-tapioca-b6dd84.netlify.app/"
+            target="_blank"
+          >
             <img src={portfolio} alt="" />
           </a>
           <div>Personal Portfolio Website With MERN Stack</div>
+        </div>
+        <div>
+          <a
+            href="https://charming-cheesecake-1a4e58.netlify.app/"
+            target="_blank"
+          >
+            <img src={Socialmedia} alt="" />
+          </a>
+          <div>Socila Media Website With MERN Stack</div>
         </div>
       </div>
     </div>
